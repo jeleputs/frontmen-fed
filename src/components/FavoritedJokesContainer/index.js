e0336cbedecd03917364340c6303d853fee717e6
jeleputs <raguilera@collethnic.com>
2019-04-29 01:52:09 -0500

    Updated favorited jokes container syles and markup

--- src/components/FavoritedJokesContainer/index.js
+++ src/components/FavoritedJokesContainer/index.js
@@ -5,7 +5,7 @@ import JokesComponent from '../JokesComponent';
 class FavoritedJokesContainer extends Component {
   render() {
     return (
-      <section>
+      <section className="jokes-container favorited-jokes-container">
         <h1>Favorited Jokes</h1>
         <JokesComponent />
       </section>
