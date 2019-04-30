bba9cccad6e8af5aaa44276580ab9c48e86b6916
jeleputs <raguilera@collethnic.com>
2019-04-30 01:02:21 -0500

    Created login container and added styles

--- src/components/LoginContainer/index.js
+++ src/components/LoginContainer/index.js
@@ -1,18 +1,79 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import './styles.scss';
 
 function LoginContainer(props) {
-  const [user, setUser] = useState({});
+  const API = 'http://localhost:3000/login';
+
+  const [formData, setFormData] = useState({ username: '', password: '' });
+  const [error, setError] = useState('');
+  const { userCredentials, setUserCredentials } = props;
+
+  useEffect(() => {
+    console.log(userCredentials);
+    if (userCredentials.token) {
+      props.history.push('/dashboard');
+    }
+  }, []);
 
   function validateLogin() {
-    props.history.push('/dashboard');
+    fetch(API, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ...formData
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.err) {
+          setError(data.err);
+        }
+        if (data.token) {
+          setUserCredentials(data);
+          setError('');
+          setFormData({ username: '', password: '' });
+          props.history.push('/dashboard');
+        }
+      });
   }
 
+  function _handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      validateLogin();
+    }
+  }
+
+  const updateFormData = event =>
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+
+  const { username, password } = formData;
+  const errorClass = error !== '' ? 'message error' : 'message';
+
   return (
-    <div className="login-form">
-      <input type="text" id="user" />
-      <input type="password" id="password" />
-      <button onClick={() => validateLogin()}>Ingresar</button>
+    <div className="login-container" onKeyPress={e => _handleKeyPress(e)}>
+      <div className="login-form">
+        <div className={errorClass}>{error}</div>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={username}
+          onChange={e => updateFormData(e)}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          onChange={e => updateFormData(e)}
+          required
+        />
+        <button onClick={() => validateLogin()}>Login</button>
+      </div>
     </div>
   );
 }
